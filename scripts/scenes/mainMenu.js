02f9d6b046a31582cb12664bc67278a575794c3f
nyoon8360 <64241860+nyoon8360@users.noreply.github.com>
2021-08-14 00:09:26 -0400

    Added connection to server. Added barebones functionality for start button. Added fetching and storing of player UUID from localstorage. pain suffering

--- scripts/scenes/mainMenu.js
+++ scripts/scenes/mainMenu.js
@@ -18,12 +18,45 @@ export default class MainMenu extends Phaser.Scene
         .on('pointerover', () => this.startButtonOver())
         .on('pointerout', () => this.startButtonOut());
 
+
+        //connect to server
+        this.socket = io('http://localhost:3000');
+        var clientSocket = this.socket;
+
+        this.socket.on('connect', function () {
+        	console.log('Connected!');
+        });
+        
+        this.socket.on('responseUUIDValidation', function(accepted, acceptedUUID) {
+            if (accepted) {
+                //new uuid was accepted
+                localStorage.setItem('playerUUID', acceptedUUID);
+                clientSocket.emit('connectWithUUID', acceptedUUID);
+            } else {
+                //new uuid was rejected. generate new uuid and resend validation request
+                var playerUUID = uuidv4();
+                clientSocket.emit('validateNewUUID', playerUUID);
+            }
+        });
+
     }
 
     startButtonPressed()
     {
         this.StartButton.setStyle({ fill: '#27ff00'})
-        this.StartButton.setText("I've been clicked.")
+
+        //gets playerUUID from local storage. if not found, generates new UUID and sends to server to check if new UUID is not already taken.
+        //todo- if new UUID is sent to server, implement listener to receive whether new UUID was accepted or not and then attempt to connect with new UUID.
+        var playerUUID = localStorage.getItem('playerUUID');
+        if (playerUUID != null) {
+            this.socket.emit('connectWithUUID', playerUUID);
+        } else {
+            playerUUID = uuidv4();
+            this.socket.emit('validateNewUUID', playerUUID);
+        }
+
+        this.StartButton.setText(playerUUID);
+        //todo- disable start button while client attempts to connect to server. re-enable if something went horribly wrong :D
     }
 
     startButtonOver()
